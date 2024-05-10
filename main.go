cfdf30fba883293e526b2451cf604fcaba1f5e9b
Guy Cherkesky <guy@cherkesky.com>
2024-05-09 20:27:49 -0500

    refactored seperate fn

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/url"
 	"os"
@@ -24,99 +25,122 @@ type JsonResponse struct {
 	Words []string `json:"words"`
 }
 
+var (
+	useInput string
+	withId   *string
+	fromTime *string
+	toTime   *string
+	withWord *string
+)
+
 func main() {
-	// set flags
-	useInput := flag.String("input", "", "s3 arn")
-	withId := flag.String("with-id", "", "id to search")
-	fromTime := flag.String("from-time", "", "from time to search")
-	toTime := flag.String("to-time", "", "to time to search")
-	withWord := flag.String("with-word", "", "word to search")
+	parseFlags()
+
+	filterMap := prepareFilters()
+
+	bucket, key := parseInput()
+
+	svc := createSession()
+
+	resp := getObject(bucket, key, svc)
+
+	defer resp.Body.Close()
+
+	gz := decompress(resp.Body)
+
+	defer gz.Close()
+
+	scanner := bufio.NewScanner(gz)
+	for scanner.Scan() {
+		processLine(scanner.Text(), filterMap)
+	}
+}
+
+func parseFlags() {
+	flag.StringVar(&useInput, "input", "", "s3 arn")
+	withId = flag.String("with-id", "", "id to search")
+	fromTime = flag.String("from-time", "", "from time to search")
+	toTime = flag.String("to-time", "", "to time to search")
+	withWord = flag.String("with-word", "", "word to search")
 	flag.Parse()
 
-	if !isFlagPassed("input") {
+	if useInput == "" {
 		log.Fatal("input flag is required (s3 arn of the bucket and key)")
 	}
+}
 
-	filterMap := map[string]func(JsonResponse, string) bool{}
-	flagMap := map[string]string{}
+func prepareFilters() map[string]func(JsonResponse, string) bool {
+	filterMap := make(map[string]func(JsonResponse, string) bool)
 
-	// filter function declaration map
-	// with-word
-	if isFlagPassed("with-word") {
-		flagMap["with-word"] = *withWord
-		filterMap["with-word"] = func(jsonResponse JsonResponse, wword string) bool {
-			isFound := false
-			for _, word := range jsonResponse.Words {
-				if word == wword {
-					isFound = true
-					break
-				}
-			}
-			if isFound {
+	if *withWord != "" {
+		filterMap["with-word"] = prepareWithWordFilter()
+	}
+
+	if *withId != "" {
+		filterMap["with-id"] = prepareWithIdFilter()
+	}
+
+	if *fromTime != "" {
+		filterMap["from-time"] = prepareFromTimeFilter()
+	}
+
+	if *toTime != "" {
+		filterMap["to-time"] = prepareToTimeFilter()
+	}
+
+	return filterMap
+}
+
+func prepareWithWordFilter() func(JsonResponse, string) bool {
+	return func(jsonResponse JsonResponse, wword string) bool {
+		for _, word := range jsonResponse.Words {
+			if word == wword {
 				return true
-			} else {
-				return false
 			}
 		}
+		return false
 	}
-	// with-id
-	if isFlagPassed("with-id") {
-		flagMap["with-id"] = *withId
-		filterMap["with-id"] = func(jsonResponse JsonResponse, wid string) bool {
-			IdintValue := 0
-			IdintValue, err := strconv.Atoi(wid)
-			if err != nil {
-				exitErrorf("unknown error occurred, %v", err)
-			}
-			if jsonResponse.Id == IdintValue {
-				return true
-			} else {
-				return false
-			}
+}
+
+func prepareWithIdFilter() func(JsonResponse, string) bool {
+	return func(jsonResponse JsonResponse, wid string) bool {
+		IdintValue, err := strconv.Atoi(wid)
+		if err != nil {
+			exitErrorf("unknown error occurred, %v", err)
 		}
+		return jsonResponse.Id == IdintValue
 	}
-	// fromTime
-	if isFlagPassed("from-time") {
-		flagMap["from-time"] = *fromTime
-		filterMap["from-time"] = func(jsonResponse JsonResponse, wfromTime string) bool {
-			isFrom := false
-			jsonRespTime, _ := time.Parse(
-				time.RFC3339, jsonResponse.Time)
-			fromTime, _ := time.Parse(
-				time.RFC3339, wfromTime)
-			isFrom = fromTime.Equal(jsonRespTime) || jsonRespTime.After(fromTime)
+}
 
-			return isFrom
-		}
+func prepareFromTimeFilter() func(JsonResponse, string) bool {
+	return func(jsonResponse JsonResponse, wfromTime string) bool {
+		jsonRespTime, _ := time.Parse(time.RFC3339, jsonResponse.Time)
+		fromTime, _ := time.Parse(time.RFC3339, wfromTime)
+		return jsonRespTime.Equal(fromTime) || jsonRespTime.After(fromTime)
 	}
+}
 
-	// toTime
-	if isFlagPassed("to-time") {
-		flagMap["to-time"] = *toTime
-		filterMap["to-time"] = func(jsonResponse JsonResponse, wtoTime string) bool {
-			isFrom := false
-			jsonRespTime, _ := time.Parse(
-				time.RFC3339, jsonResponse.Time)
-			fromTime, _ := time.Parse(
-				time.RFC3339, wtoTime)
-			isFrom = fromTime.Equal(jsonRespTime) || jsonRespTime.Before(fromTime)
-
-			return isFrom
-		}
+func prepareToTimeFilter() func(JsonResponse, string) bool {
+	return func(jsonResponse JsonResponse, wtoTime string) bool {
+		jsonRespTime, _ := time.Parse(time.RFC3339, jsonResponse.Time)
+		toTime, _ := time.Parse(time.RFC3339, wtoTime)
+		return jsonRespTime.Equal(toTime) || jsonRespTime.Before(toTime)
 	}
+}
 
-	// parse input
-	u, _ := url.Parse(*useInput)
-	bucket := u.Host
-	key := u.Path
+func parseInput() (string, string) {
+	u, _ := url.Parse(useInput)
+	return u.Host, u.Path
+}
 
-	// create session
+func createSession() *s3.S3 {
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
 	})
-	svc := s3.New(sess)
+	return s3.New(sess)
+}
 
-	// get object
+func getObject(bucket, key string, svc *s3.S3) *s3.GetObjectOutput {
 	resp, err := svc.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
@@ -133,46 +157,52 @@ func main() {
 		}
 		exitErrorf("unknown error occurred, %v", err)
 	}
-	defer resp.Body.Close()
 
-	// decompress
-	gz, err := gzip.NewReader(resp.Body)
+	return resp
+}
+
+func decompress(body io.Reader) *gzip.Reader {
+	gz, err := gzip.NewReader(body)
 	if err != nil {
 		exitErrorf("unknown error occurred, %v", err)
 	}
-	defer gz.Close()
+	return gz
+}
 
-	// read line by line
-	scanner := bufio.NewScanner(gz)
-	for scanner.Scan() {
-		data := []byte(scanner.Text())
-		jsonResponse := JsonResponse{}
-		json.Unmarshal(data, &jsonResponse)
+func processLine(line string, filterMap map[string]func(JsonResponse, string) bool) {
+	data := []byte(line)
+	jsonResponse := JsonResponse{}
+	json.Unmarshal(data, &jsonResponse)
 
-		// filtering
-		hasAllFlags := true
-		for filter := range filterMap {
-			if !filterMap[filter](jsonResponse, flagMap[filter]) {
-				hasAllFlags = false
-			}
+	hasAllFlags := true
+	for filter := range filterMap {
+		if !filterMap[filter](jsonResponse, getFlagValue(filter)) {
+			hasAllFlags = false
 		}
-		if hasAllFlags {
-			fmt.Println(jsonResponse)
-		}
+	}
+	if hasAllFlags {
+		fmt.Println(jsonResponse)
+	}
+}
+
+func getFlagValue(name string) string {
+	switch name {
+	case "input":
+		return useInput
+	case "with-id":
+		return *withId
+	case "from-time":
+		return *fromTime
+	case "to-time":
+		return *toTime
+	case "with-word":
+		return *withWord
+	default:
+		return ""
 	}
 }
 
 func exitErrorf(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
 	os.Exit(1)
-}
-
-func isFlagPassed(name string) bool {
-	found := false
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-	return found
 }
